84e9ca551b259ad0762ae69f7f444cc3b94e63d4
BoukemacheAyoub <137456404+BoukemacheAyoub@users.noreply.github.com>
2024-04-20 15:52:08 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    // date_default_timezone_set('Your/Timezone');
+
+    // Get the current time in the desired format
+    $current_time = date('H:i:sa');
+
+    // Output the current time
+    echo 'Current Time: ' . $current_time;
+?>
